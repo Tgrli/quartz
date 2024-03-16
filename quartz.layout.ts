606b54c13fc04eb8f80307c4f77c5683245a34b0
tgr <kl3010@nyu.edu>
2024-03-17 00:32:22 +0800

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/KyanStack",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      Twitter: "https://twitter.com/KyanStack", 
+      
     },
   }),
 }
